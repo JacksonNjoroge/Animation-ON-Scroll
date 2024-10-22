37f555ae6ffe59a0647f5d985201f773d9e551d9
JacksonNjoroge <jacknjooro254@gmail.com>
2024-10-21 23:43:34 -0700

    Animation Scroll Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let sections = document.querySelectorAll('section')
+
+window.onscroll = () => {
+    sections.forEach((section)=>{
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            section.classList.add('show-animate')
+        }else{
+            section.classList.remove('show-animate');
+        }
+    })
+}
